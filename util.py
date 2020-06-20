12f1fafa450418e120f349a0ccc5003ba3a96690
kihongMin <minkh93@gmail.com>
2020-06-20 15:48:34 +0900

    modify error

--- util.py
+++ util.py
@@ -13,25 +13,25 @@ class Preprocess:
         #self.test = pd.read_json(test_path, typ='frame')
         self.concat_train_val()
         self.min_value = min_value
-        self.tag_encoder, self.tag_decoder = makeDict(self.total_plylst['tags'],min_value)
-        self.song_encoder, self.song_decoder = makeDict(self.total_plylst['songs'],min_value)
-        self.plylst_encoder, self.plylst_decoder = makeDict(self.total_plylst['id'])
+        self.tag_encoder, self.tag_decoder = self.makeDict(self.total_plylst['tags'],min_value)
+        self.song_encoder, self.song_decoder = self.makeDict(self.total_plylst['songs'],min_value)
+        self.plylst_encoder, self.plylst_decoder = self.makeDict(self.total_plylst['id'])
 
     def __call__(self):
-        self.plylst['songs'] = self.plylst['songs'].apply(lambda x: list(map(lambda y:self.song_encoder[y],list(filter(lambda z: z in self.song_encoder,x)))))
-        self.plylst['tags'] = self.plylst['tags'].apply(lambda x: list(map(lambda y:self.tag_encoder[y],list(filter(lambda z: z in self.tag_encoder,x)))))
-        self.plylst['ids'] = self.plylst['id'].apply(lambda x: list(map(lambda y:self.plylst_encoder[y],list(filter(lambda z: z in self.plylst_encoder,x)))))
+        self.total_plylst['songs'] = self.total_plylst['songs'].apply(lambda x: list(map(lambda y:self.song_encoder[y],list(filter(lambda z: z in self.song_encoder,x)))))
+        self.total_plylst['tags'] = self.total_plylst['tags'].apply(lambda x: list(map(lambda y:self.tag_encoder[y],list(filter(lambda z: z in self.tag_encoder,x)))))
+        self.total_plylst['id'] = self.total_plylst['id'].apply(lambda x: self.plylst_encoder[x])
 
-        return self.plylst[['songs','tags','ids']]
+        return self.total_plylst[['songs','tags','id']]
 
     def concat_train_val(self):
         self.train['istrain'] = 1
         self.val['istrain'] = 0
         self.n_train = len(self.train)
         self.n_val = len(self.val)
-        self.total_plylst = pd.concat([self.train,self.test],ignore_index=True)
+        self.total_plylst = pd.concat([self.train,self.val],ignore_index=True)
 
-    def makeDict(df, min_value=0):
+    def makeDict(self,df, min_value=0):
         c = Counter()
         #if isinstance(df, pd.DataFrame):
         if isinstance(df[0], list):
